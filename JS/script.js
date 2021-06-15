39b9dde47b0a503d7e12b067dbe60a8fe8e84598
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-15 17:15:02 +0200

    codice

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// ---1---
+// Creare un oggetto che descriva uno studente,
+// con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in
+// tutte le proprietà dell’oggetto.
+var nameStudent={
+    name:'Carlos',
+    surname:'Ferrari',
+    age:'30'
+}
+console.log(nameStudent)
+var resultOggett = document.getElementById('output');
+for(var x in nameStudente){
+    resultOggett.innetHTML += x  +':'+ nameStudent[x]
+}
